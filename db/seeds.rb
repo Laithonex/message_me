2b90ac6c2adcc1710aabda257e5f924a80922014
Laithonex <laith.aljufaili966@gmail.com>
2020-03-22 08:10:23 +0400

    Add messages from UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Laith", password:"password")
-User.create(username: "Saif", password:"password")
-User.create(username: "Said", password:"password")
-User.create(username: "Ahmed", password:"password")
+Message.create(body: "Be yourself; everyone else is already taken.", user_id:"1")
+Message.create(body: "So many books, so little time.", user_id:"2")
+Message.create(body: "A room without books is like a body without a soul.", user_id:"3")
+Message.create(body: "You only live once, but if you do it right, once is enough.", user_id:"4")
+Message.create(body: "Be the change that you wish to see in the world.", user_id:"1")
+Message.create(body: "No one can make you feel inferior without your consent.", user_id:"1")
